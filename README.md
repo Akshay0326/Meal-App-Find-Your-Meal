282755d4838aa94bf246bbb6daabd5feb0f300d8
Akshay0326 <akshaykumarsinha0326@gmail.com>
2023-01-20 15:45:51 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,63 @@
+---
+About the Project :-
+
+🔴 This is a single page website where you can seach about any meal.
+
+🔴 You can also add it into on your favourite list.
+
+🔴 The most important thing about this project it is fully responsive of nature and use an api called the themealdb to fetch all the dishes.
+
+🔴 It also redirects users to a youtube videos for the recipe of dishes.
+---
+
+📐 Installation :-
+
+Nothing crazy just download the zip and run LIVE_SERVER.
+
+---
+
+🪛 Built With :-
+
+🟠 HTML
+🟠 Pure CSS
+🟠 Vanilla Javascript
+🟠 Bootstrap
+
+---
+
+💻 Screenshots :-
+
+🔴 Landing Page:-
+
+![Screenshot 2023-01-20 130400](https://user-images.githubusercontent.com/86068467/213666054-b4ea0a5b-ae40-4a92-8f67-69368fec4143.png)
+
+🔴 Home Page :-
+
+![Homepage1](https://user-images.githubusercontent.com/86068467/213669017-5c2dd093-1e81-44f9-a1a5-47aebfa8f7af.png)
+
+![Homepage2](https://user-images.githubusercontent.com/86068467/213669113-0775d71d-8805-48e0-af0e-1d084988a947.png)
+
+🔴 Favourite Section :-
+
+![Favourite](https://user-images.githubusercontent.com/86068467/213669219-9e6fdf29-d26f-42ef-91a0-617afdf25b70.png)
+
+🔴 Recipe Instruction Page :-
+
+![InstructionPage](https://user-images.githubusercontent.com/86068467/213669374-3983f32b-278b-478a-a150-905ee9e66ce9.png)
+
+---
+
+🚦Versions:-
+
+    1.0.0
+
+---
+
+🙎 Contact .
+
+    🔗 Author - @Akshay Kumar - akshaykumarsinha0326@gmail.com - +91-7562863220
+
+    ✅ Linkedin ✅ https://www.linkedin.com/in/itsakshaykrs/ ✅
+    ✅ Github   ✅ https://github.com/Akshay0326             ✅
+
+---
---
+++ script.js
@@ -0,0 +1,182 @@
+// it makes a favourites meal array if its not exist in local storage
+if (localStorage.getItem("favouritesList") == null) {
+    localStorage.setItem("favouritesList", JSON.stringify([]));
+}
+
+// it fetches meals from api and return it
+async function fetchMealsFromApi(url, value) {
+    const response = await fetch(`${url + value}`);
+    const meals = await response.json();
+    return meals;
+}
+
+
+
+// it show's all meals card in main acording to search input value
+function showMealList() {
+    let inputValue = document.getElementById("my-search").value;
+    let arr = JSON.parse(localStorage.getItem("favouritesList"));
+    let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+    let html = "";
+    let meals = fetchMealsFromApi(url, inputValue);
+    meals.then(data => {
+        if (data.meals) {
+            data.meals.forEach((element) => {
+                let isFav = false;
+                for (let index = 0; index < arr.length; index++) {
+                    if (arr[index] == element.idMeal) {
+                        isFav = true;
+                    }
+                }
+                if (isFav) {
+                    html += `
+                <div id="card" class="card mb-3" style="width: 20rem;">
+                    <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.strMeal}</h5>
+                        <div class="d-flex justify-content-between mt-5">
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">More Details</button>
+                            <button id="main${element.idMeal}" class="btn btn-outline-light active" onclick="addRemoveToFavList(${element.idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+                } else {
+                    html += `
+                <div id="card" class="card mb-3" style="width: 20rem;">
+                    <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.strMeal}</h5>
+                        <div class="d-flex justify-content-between mt-5">
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">More Details</button>
+                            <button id="main${element.idMeal}" class="btn btn-outline-light" onclick="addRemoveToFavList(${element.idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+                }
+            });
+        } else {
+            html += `
+            <div class="page-wrap d-flex flex-row align-items-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12 text-center">
+                            <span class="display-1 d-block">404</span>
+                            <div class="mb-4 lead">
+                                The meal you are looking for was not found.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+        document.getElementById("main").innerHTML = html;
+    });
+}
+
+
+
+//it shows the full meal details in main
+async function showMealDetails(id) {
+    let url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+    let html = "";
+    await fetchMealsFromApi(url, id).then(data => {
+        html += `
+          <div id="meal-details" class="mb-5">
+            <div id="meal-header" class="d-flex justify-content-around flex-wrap">
+              <div id="meal-thumbail">
+                <img class="mb-2" src="${data.meals[0].strMealThumb}" alt="" srcset="">
+              </div>
+              <div id="details">
+                <h3>${data.meals[0].strMeal}</h3>
+                <h6>Category : ${data.meals[0].strCategory}</h6>
+                <h6>Area : ${data.meals[0].strArea}</h6>
+              </div>
+            </div>
+            <div id="meal-instruction" class="mt-3">
+              <h5 class="text-center">Instruction :</h5>
+              <p>${data.meals[0].strInstructions}</p>
+            </div>
+            <div class="text-center">
+              <a href="${data.meals[0].strYoutube}" target="_blank" class="btn btn-outline-light mt-3">Watch Video</a>
+            </div>
+          </div>
+        `;
+    });
+    document.getElementById("main").innerHTML = html;
+}
+
+
+
+
+// its shows all favourites meals in favourites body
+async function showFavMealList() {
+    let arr = JSON.parse(localStorage.getItem("favouritesList"));
+    let url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+    let html = "";
+    if (arr.length == 0) {
+        html += `
+            <div class="page-wrap d-flex flex-row align-items-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12 text-center">
+                            <span class="display-1 d-block">404</span>
+                            <div class="mb-4 lead">
+                                No meal added in your favourites list.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+    } else {
+        for (let index = 0; index < arr.length; index++) {
+            await fetchMealsFromApi(url, arr[index]).then(data => {
+                html += `
+                <div id="card" class="card mb-3" style="width: 20rem;">
+                    <img src="${data.meals[0].strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.meals[0].strMeal}</h5>
+                        <div class="d-flex justify-content-between mt-5">
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${data.meals[0].idMeal})">More Details</button>
+                            <button id="main${data.meals[0].idMeal}" class="btn btn-outline-light active" onclick="addRemoveToFavList(${data.meals[0].idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+            });
+        }
+    }
+    document.getElementById("favourites-body").innerHTML = html;
+}
+
+
+
+
+
+
+//its adds and remove meals to favourites list
+function addRemoveToFavList(id) {
+    let arr = JSON.parse(localStorage.getItem("favouritesList"));
+    let contain = false;
+    for (let index = 0; index < arr.length; index++) {
+        if (id == arr[index]) {
+            contain = true;
+        }
+    }
+    if (contain) {
+        let number = arr.indexOf(id);
+        arr.splice(number, 1);
+        alert("your meal removed from your favourites list");
+    } else {
+        arr.push(id);
+        alert("your meal add your favourites list");
+    }
+    localStorage.setItem("favouritesList", JSON.stringify(arr));
+    showMealList();
+    showFavMealList();
+}
+
+
